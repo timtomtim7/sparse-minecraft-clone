89738646246f51a5b94678920b5c43b57644aba6
BuildTools <tomhazredstone@hotmail.com>
2018-03-17 13:50:58 -0500

    Transparency is... annoying

--- src/main/kotlin/blue/sparse/minecraft/client/gui/TestGUI.kt
+++ src/main/kotlin/blue/sparse/minecraft/client/gui/TestGUI.kt
@@ -32,20 +32,20 @@ object TestGUI : GUI() {
 		drawRectangle("icons/exp_empty", hotbarLeft, 24f, 182f, 5f)
 		drawRectangle("icons/exp_full", hotbarLeft, 24f, 182f * exp, 5f, exp)
 
-		for(i in 0 until 10) {
+		for (i in 0 until 10) {
 			drawRectangle("icons/heart_black_outline", hotbarLeft + (i * 8), 30f, 9f)
 			drawRectangle("icons/heart_full", hotbarLeft + (i * 8), 30f, 9f)
 		}
 
-		for(i in 0 until 10) {
-			drawRectangle("icons/food_black_outline", hotbarRight - ((i+1) * 8) - 1, 30f, 9f)
-			drawRectangle("icons/food_full", hotbarRight - ((i+1) * 8) - 1, 30f, 9f)
+		for (i in 0 until 10) {
+			drawRectangle("icons/food_black_outline", hotbarRight - ((i + 1) * 8) - 1, 30f, 9f)
+			drawRectangle("icons/food_full", hotbarRight - ((i + 1) * 8) - 1, 30f, 9f)
 		}
 
 		glCall { glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_ONE_MINUS_SRC_ALPHA) }
-//		glCall { glBlendEquation(GL_FUNC_SUBTRACT) }
+
 		drawRectangle("icons/crosshair", manager.right / 2f - 16f / 2f, manager.top / 2f - 16f / 2f, 16f)
-//		glCall { glBlendEquation(GL_FUNC_ADD) }
+
 		glCall { glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA) }
 
 		drawString(String.format("FPS: %.1f", SparseEngine.frameRate), 1f, manager.top - 9)
